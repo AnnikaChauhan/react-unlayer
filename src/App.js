7371111299741c7fcbfb2b356deb3cd1255ae979
AnnikaChauhan <annikac0711@gmail.com>
2021-01-26 09:42:57 +0000

    progress so far including theme change, language change, style changes and use of methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
 
-function App() {
+import EmailEditor from "react-email-editor";
+
+const App = () => {
+  const emailEditorRef = useRef(null);
+
+  const exportHtml = () => {
+    emailEditorRef.current.editor.exportHtml((data) => {
+      const { design, html } = data;
+      console.log(data);
+      // console.log("exportHtml", html);
+    });
+  };
+
+  const onLoad = () => {
+    // we will need to save the template in JSON format to the marketing API, then call it back in JSON format to show the user an existing template - for editing
+    // Can we ask Miodrag if they can take the entire JSON object and use just the HTML to send the template?
+    const templateJson = {};
+
+    if (Object.keys(templateJson).length !== 0) {
+      emailEditorRef.current.editor.loadDesign(templateJson);
+    }
+    return null;
+  };
+
+  const save = () => {
+    emailEditorRef.current.editor.saveDesign((data) => {
+      // this data is just the design part of the JSON
+      console.log(data);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Unlayer</h1>
+      <div
+        style={{
+          padding: "10px",
+          width: "33%",
+          display: "flex",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <button onClick={exportHtml}>Export HTML</button>
+        <button onClick={save}>Save Design</button>
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          overflow: "scroll",
+        }}
+      >
+        <EmailEditor
+          ref={emailEditorRef}
+          minHeight="500px"
+          style={{ border: "1px solid red" }}
+          onLoad={onLoad}
+          // locale="it"
+          locale="en"
+          safeHtml={true}
+          appearance={{
+            theme: "dark",
+            panels: {
+              tools: {
+                dock: "left",
+              },
+            },
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
